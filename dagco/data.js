499bb412b8b558467c0eb026e6164cf9107d32dc
pip-eline <pip-eline>
2023-09-14 16:33:10 +0200

    dag

---
+++ dagco/data.js
@@ -0,0 +1,210 @@
+var data = [
+    {
+        id: `b-algemene-broadcast`,
+        nederlands: {
+            titel: `Heb je een team?`,
+            beschrijving: `Zorg dat je hulp hebt, een integrator, een dag coordinator, RST support, Wellbeing`,
+            toelichting: `Organiseer het niet in je eentje. een team van 3 of meer is helemaal niet raar. dat is nog los van de vingerco's die je evt nodig hebt.`,
+        },
+        engels: {
+            titel: `Join the GENERAL BROADCAST`,
+            beschrijving: `It's <a href="https://t.me/A12StopSub">this telegram group</a> with the title "A12 StopSub".`,
+            toelichting: `For most actions, including the <em>A12 actions</em>, there is a big broadcast channel where important announcement are made.`,
+        }
+    },
+    // {
+    //     id: `b-aanmeldformulier`,
+    //     nederlands: {
+    //         titel: `Vul het AANMELDFORMULIER in`,
+    //         beschrijving: `Het <a href="https://extinctionrebellion.nl/a12aanmelden">aanmeldformulier</a> zorgt er voor dat je in de juiste groepen wordt ingedeeld, zowel voor de datum van 9 september als de dagen daarna.`,
+    //         toelichting: `Je kan het formulier later nog aanpassen als je op meer/andere dagen komt. In het formulier kan je ook aangeven of je bepaalde rollen wil vervullen, zoals wellbeing.`,
+    //     },
+    //     engels: {
+    //         titel: `Fill in the REGISTRATION FORM`,
+    //         beschrijving: `The <a href="https://extinctionrebellion.nl/a12aanmelden">registration form</a> makes sure you are assigned to the right groups, both for the 9th of September, as well as after.`,
+    //         toelichting: `You can edit your form at a later for different/additional dates. In the form you can also indicate preferences for specific roles, like wellbeing.`,
+    //     }
+    // },
+    //     {
+    //     id: `b-actietraining`,
+    //     nederlands: {
+    //         titel: `Volg een ACTIETRAINING`,
+    //         beschrijving: `<a href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Hier staat een overzicht</a> van alle actietrainingen die nog gegeven worden.`,
+    //         toelichting: `Dit heet ook wel een <em>"non violent direct action training"</em>. Als je dat al eerder hebt gedaan, hoeft dat niet nogmaals. Maar als je nieuw bent, is een actietraining heel belangrijk: doe niet mee zonder actietraining! Je kan dan wel nog meedoen aan de <a href="https://t.me/A12supporters">support demo</a>.`,
+    //     },
+    //     engels: {
+    //         titel: `Do an ACTION TRAINING`,
+    //         beschrijving: `<a href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Here is an overview</a> of all the trainings being organised.`,
+    //         toelichting: `This is also called a <em>"non violent direct action training"</em>. If you've done one before, you don't need to do it again. However, if this is your first action, this training is very important: you shouldn't go without one! Without a training you can still join the <a href="https://t.me/A12supporters">support demo</a>.`,
+    //     }
+    // },
+    // {
+    //     id: `b-vingergroep`,
+    //     nederlands: {
+    //         titel: `Ga in de VINGERGROEP`,
+    //         beschrijving: `Nadat je het formulier hebt ingevuld, wordt je ingedeeld bij een telegram- of signalgroep voor jouw vinger.`,
+    //         toelichting: `Bij grote acties wordt er soms gewerkt met 'vingers': dat zijn groepen die samen voorbereiden en naar de actielocatie lopen. Dit vergroot de veiligheid en de kans dat zo veel mogelijk rebellen op de actie aankomen.<br/><br/>TOEVOEGING: Omdat deze actie meerdere dagen duurt, kan het zijn dat je lid bent van meerdere vingers voor verschillende data.`,
+    //     },
+    //     engels: {
+    //         titel: `Join the FINGER GROUP`,
+    //         beschrijving: `After you have filled out the form, you will be invited to a telegram or signal group for your specific finger.`,
+    //         toelichting: `Larger actions sometimes use 'fingers'. These are groups that prepare for the action together and move towards the place of action together. This increases security and our chances of succes.<br/><br/>ADDENDUM: Because this action lasts multiple days, you may be part of multiple fingers for different dates.`,
+    //     }
+    // },
+    // {
+    //     id: `b-actieconsensus`,
+    //     nederlands: {
+    //         titel: `Lees de ACTIECONSENSUS`,
+    //         beschrijving: `Dit is heel belangrijk. Lees de actie consensus zorgvuldig. Als je het ergens niet mee eens ben, GA DAN NIET MEE. <a href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">Je kunt het hier lezen.</a>`,
+    //         toelichting: `De actie consensusbevat de basis van alle voorwaarden die we hebben afgesproken als Extinction Rebellion, waaronder geweldloosheid.<br/>Ben je al een meer ervaren rebel? Spreek andere rebellen gerust vriendelijk aan als je ziet dat ze de actieconsensus niet volgen.`,
+    //     },
+    //     engels: {
+    //         titel: `Read the ACTION CONSESUS`,
+    //         beschrijving: `This is very important. Read the action consensus carefully. If you don't agree, DON'T JOIN. <a href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">You can read it here</a>`,
+    //         toelichting: `The Action Consensus contains all the conditions we agreed upon as Extinction Rebellion, including non-violence.<br/>If you're a more experienced rebel, feel free to kindly point it out when you see other rebels going against the action consensus.`,
+    //     }
+    // },
+    // {
+    //     id: `b-legal-handout`,
+    //     nederlands: {
+    //         titel: `Lees de LEGAL HANDHOUT`,
+    //         beschrijving: `De legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM/download/A12-Juridische%20Informatie-FINAL-UPDATEv2.pdf">staat hier</a>`,
+    //         toelichting: `Hierin staan antwoorden op vragen zoals: Wat zijn de legale consequenties? Wanneer bel ik een advocaat? Betekent dit iets voor mijn strafblad?`,
+    //     },
+    //     engels: {
+    //         titel: `Read the LEGAL HANDOUT`,
+    //         beschrijving: `The legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM/download/A12-Juridische%20Informatie-FINAL-UPDATEv2.pdf">can be read here</a-->.`,
+    //         toelichting: `This handout answers questions like: What are the legal consequenses? When do I call a laywer?`,
+    //     }
+    // },
+    // {
+    //     id: `b-risico-niveau`,
+    //     nederlands: {
+    //         titel: `Bepaal: HIGH RISK of LOW RISK`,
+    //         beschrijving: `Twijfel je? Bespreek de voor- en nadelen met anderen.`,
+    //         toelichting: `Het is prettig om een "Plan A" te hebben. Zodat je weet wat je wil, ook in stressvolle/hectische situaties.<br/>Bij low risk zeg je: "ik ga weg als de politie vordert". De kans dat je gearresteerd wordt, is dan heel klein.`,
+    //     },
+    //     engels: {
+    //         titel: `Commit: HIGH RISK or LOW RISK`,
+    //         beschrijving: `Still on the fence? Discuss the pros and cons with others.`,
+    //         toelichting: `We always recommend you have a clear "Plan A". If you make that decision early, you'll know what to do in stressful/hectic situations.<br/>Low risk means: "I will leave when the police ask me to". The chance of arrest be very small in that case.`,
+    //     }
+    // },
+    // {
+    //     id: `b-anonimiteit`,
+    //     nederlands: {
+    //         titel: `Bepaal: ANONIEM of niet?`,
+    //         beschrijving: `Er zijn diverse voor- en nadelen dus laat je vooral goed informeren. Voor meer information, vraag de mede-rebellen of de vingercoordinator in je vingergroep. <a href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Hier is ook een document met meer informatie</a>`,
+    //         toelichting: `Als je anoniem gaat, neem dan ook geen spullen mee met jouw naam er op. Dus ook geen bankpas of OV-chipkaart op naam.`,
+    //     },
+    //     engels: {
+    //         titel: `Consider: Will I go ANONYMOUS?`,
+    //         beschrijving: `There are various pros and cons for anonymity, so inform yourself well. For more information, ask other rebels or the finger coordinator in your finger group. <a href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Here is a document with more information</a>`,
+    //         toelichting: `If you go anonymous, don't bring any stuff with your name on it. That also means no debit card or named OV card.`,
+    //     }
+    // },
+    // {
+    //     id: `b-algemene-briefing`,
+    //     nederlands: {
+    //         titel: `Volg de ALGEMENE BRIEFING`,
+    //         beschrijving: `Actiebriefing gemist? <a href="https://briefing.a12blokkade.nl/">briefing.a12blokkade.nl</a>`,
+    //         toelichting: `De algemene briefing geeft een laatste update en herhaalt de belangrijkste punten. Het is een zoom-meeting met honderden deelnemers, dus er is niet veel ruimte voor vragen. Wel kan je deze in je vingergroep vragen.<br/><br/>TOEVOEGING: Voor data ná 9 september zijn er steeds briefings op de avond ervóór.`,
+    //     },
+    //     engels: {
+    //         titel: `Follow the GENERAL BRIEFINGS`,
+    //         beschrijving: `Missed the briefing? <a href="https://briefing.a12blokkade.nl/">briefing.a12blokkade.nl</a>`,
+    //         toelichting: `The general briefing provides the most recent updates and repeats the most import points. It's huge zoom meeting, so there's not a lot of time for questions. If you have remaining questions, you can always ask them in your finger group.<br/><br/>ADDENDUM: For dates after September 9th, briefings will be held the evening prior.`,
+    //     }
+    // },
+    // {
+    //     id: `b-vingerbriefing`,
+    //     nederlands: {
+    //         titel: `Volg de VINGERBRIEFING`,
+    //         beschrijving: `Je vinger organiseert waarschijnlijk ook nog een eigen vingerbriefing. Houd de groep in de gaten en doe hieraan mee!`,
+    //         toelichting: `Hier hoor je belangrijke informatie zoals de verzamellocatie en aanlooproute voor jouw vinger. Bovendien is er tijd voor vragen, en kunnen er nog buddies gemaakt worden.`,
+    //     },
+    //     engels: {
+    //         titel: `Join the FINGER BRIEFING`,
+    //         beschrijving: `Your finger will probably organise a separate finger briefing. Keep an eye out for this and be sure to participate.`,
+    //         toelichting: `This is where you will hear important information such as the meeting point and route of approach. In addition, you can ask questions and there will still be time to make buddies.`,
+    //     }
+    // },
+    // {
+    //     id: `b-rst-nummer`,
+    //     nederlands: {
+    //         titel: `Trek een RST-NUMMER`,
+    //         beschrijving: `Trek je RST-nummer. Doe dit altijd, ook als je low-risk gaat. <b>De link waarmee je dat doet, krijg je van je vingercoordinator, in je vingergroep</b>.`,
+    //         toelichting: `Trek maar 1x een nummer en onthoud deze goed! Dit nummer blijft geldig voor alle dagen van de permanente blokkade!<br/><br/>Het RST-nummer is de anonieme koppeling tussen jou en jouw gegevens. Alleen RST support weet wie er achter elk nummer zit, en zal de informatie na de actie verwijderen.`,
+    //     },
+    //     engels: {
+    //         titel: `Get and RST-NUMBER`,
+    //         beschrijving: `Draw an RST-number. Do this always, also if you're going low risk. <b>You will receive the link with which you do this from your fingercoordinator in your finger group.</b>`,
+    //         toelichting: `Draw your number only once and remember it well! This number remains valid for all days of the permanent blockade!<br/><br/>The RST number is used to connect you anonymously to who you are. Only RST support knows who is associated with what number, and will delete this information after the action.`,
+    //     }
+    // },
+    //     {
+    //     id: `b-rst-formulier`,
+    //     nederlands: {
+    //         titel: `Vul het RST formulier in!`,
+    //         beschrijving: `Het RST formulier wordt gedeeld door je vingercoordinator, in je vingergroep.`,
+    //         toelichting: `Je RST-nummer zorgt er voor dat arrestee-support (RSTS) jou kan traceren. Zo weten ze of er nog rebellen vast zitten. Achter de schermen werkt RST-support er hard aan om iedereen weer vrij te krijgen.`,
+    //     },
+    //     engels: {
+    //         titel: `Fill in the RST-form!`,
+    //         beschrijving: `The RST-form will be shared by your finger coordinator, in your finger group.`,
+    //         toelichting: `Your RST-number is used to trace you during the action. In this way arrestee support (RSTS) knows if people are still being detained. RST support is working very hard to get you out and support you!`,
+    //     }
+    // },
+    // {
+    //     id: `b-tas-inpakken`,
+    //     nederlands: {
+    //         titel: `Pak je TAS in`,
+    //         beschrijving: `Een paklijst is <a href="https://cloud.extinctionrebellion.nl/index.php/s/iLB8YA5icBmwNag">hier beschikbaar</a>.`,
+    //         toelichting: `Denk in elk geval aan: warme/droge kleren, eten, WATER, geld, ID-kaart*, OV-kaart*. Géén dierbare bezittingen. Géén wapens, géén drugs.<br/>* niet als je anoniem gaat`,
+    //     },
+    //     engels: {
+    //         titel: `Pack your BAG!`,
+    //         beschrijving: `A packing list is available <a href="https://cloud.extinctionrebellion.nl/index.php/s/iLB8YA5icBmwNag">here</a>.`,
+    //         toelichting: `Make sure you at least have enough warm/dry clothing, food, WATER, ID-card*, OV-card*. Don't bring precious things. No alcohol, no drugs.<br/>* not when going anonymously`,
+    //     }
+    // },
+    // {
+    //     id: `b-plan-treinreis`,
+    //     nederlands: {
+    //         titel: `Plan je (TREIN)REIS`,
+    //         beschrijving: `Mogelijk deelt jouw vinger nog een reisadvies.`,
+    //         toelichting: `Houd rekening met eventuele vertragingen. Reis in kleine groepjes of alleen.`,
+    //     },
+    //     engels: {
+    //         titel: `Plan your JOURNEY`,
+    //         beschrijving: `Most fingers will share some travel advice`,
+    //         toelichting: `Account for delays in public transport. Travel in small groups or alone.`,
+    //     }
+    // },
+    // {
+    //     id: `b-watervaste-stift`,
+    //     nederlands: {
+    //         titel: `Schrijf dit op je LICHAAM`,
+    //         beschrijving: `Schrijf met <em>watervaste stift</em> twee dingen duidelijk op je lichaam:<br/>1. Naam van de advocaat: Willem Jebbink.<br/>2. Je RST-nummer.`,
+    //         toelichting: `De naam van de advocaat is Willem Jebbink. Een telefoonnummer is niet nodig.`,
+    //     },
+    //     engels: {
+    //         titel: `Write this on your BODY`,
+    //         beschrijving: `Use a permanent marker to write these two things on your body:<br/>1. Name of your lawyer: Willem Jebbink.<br/>2. Your RST-number.`,
+    //         toelichting: `The name of your laywer is Willem Jebbink. A phone number is not needed.`,
+    //     }
+    // },
+    // {
+    //     id: `a-zen`,
+    //     nederlands: {
+    //         titel: `Je bent er KLAAR voor!`,
+    //         beschrijving: `Neem nog wat rust. Bedenk waarom je het ook alweer doet. We hebben een goed verhaal, we zijn met z'n duizenden en groeien alleen maar. Misschien vind je het spannend. Maar probeer ook vooral plezier te hebben.`,
+    //         toelichting: `Tot snel :)`,
+    //     },
+    //     engels: {
+    //         titel: `You are READY to go!`,
+    //         beschrijving: `Take a deep breath, remember why you are doing this. We're doing this for an important cause, and we're getting more support every day. Maybe you're anxious. But try to have a bit of fun too.`,
+    //         toelichting: `We'll see you soon! :)`,
+    //     }
+    // },
+]
