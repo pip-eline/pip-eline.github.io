bfac19fc88e468bb970af9a07485ad06d6d70054
pip-eline <133957473+pip-eline@users.noreply.github.com>
2024-01-24 11:07:25 +0100

    Update data.js

--- checklist/data.js
+++ checklist/data.js
@@ -16,13 +16,13 @@ var data = [
         id: `b-aanmeldformulier`,
         nederlands: {
             titel: `Vul het AANMELDFORMULIER in`,
-            beschrijving: `Het <a href="https://extinctionrebellion.nl/a12aanmelden">aanmeldformulier</a> zorgt er voor dat je in de juiste groepen wordt ingedeeld, zowel voor de datum van 9 september als de dagen daarna.`,
-            toelichting: `Je kan het formulier later nog aanpassen als je op meer/andere dagen komt. In het formulier kan je ook aangeven of je bepaalde rollen wil vervullen, zoals wellbeing.`,
+            beschrijving: `Het <a href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">aanmeldformulier</a> zorgt er voor dat je in de juiste groep wordt ingedeeld, voor verdere communicatie.`,
+            toelichting: `In het formulier kan je ook aangeven of je bepaalde rollen wil vervullen, zoals wellbeing.`,
         },
         engels: {
             titel: `Fill in the REGISTRATION FORM`,
-            beschrijving: `The <a href="https://extinctionrebellion.nl/a12aanmelden">registration form</a> makes sure you are assigned to the right groups, both for the 9th of September, as well as after.`,
-            toelichting: `You can edit your form at a later for different/additional dates. In the form you can also indicate preferences for specific roles, like wellbeing.`,
+            beschrijving: `The <a href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">registration form</a> makes sure you are assigned to the right group, for further communication.`,
+            toelichting: `In the form you can also indicate preferences for specific roles, like wellbeing.`,
         }
     },
         {
@@ -34,7 +34,7 @@ var data = [
         },
         engels: {
             titel: `Do an ACTION TRAINING`,
-            beschrijving: `<a href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Here is an overview</a> of all the trainings being organised.`,
+            beschrijving: `<a href="https://extinctionrebellion.nl/en/events/?category=Actietraining&organizer">Here is an overview</a> of all the trainings being organised.`,
             toelichting: `This is also called a <em>"non violent direct action training"</em>. If you've done one before, you don't need to do it again. However, if this is your first action, this training is very important: you shouldn't go without one! Without a training you can still join the <a href="https://t.me/A12supporters">support demo</a>.`,
         }
     },
@@ -43,12 +43,12 @@ var data = [
         nederlands: {
             titel: `Ga in de VINGERGROEP`,
             beschrijving: `Nadat je het formulier hebt ingevuld, wordt je ingedeeld bij een telegram- of signalgroep voor jouw vinger.`,
-            toelichting: `Bij grote acties wordt er soms gewerkt met 'vingers': dat zijn groepen die samen voorbereiden en naar de actielocatie lopen. Dit vergroot de veiligheid en de kans dat zo veel mogelijk rebellen op de actie aankomen.<br/><br/>TOEVOEGING: Omdat deze actie meerdere dagen duurt, kan het zijn dat je lid bent van meerdere vingers voor verschillende data.`,
+            toelichting: ``,
         },
         engels: {
             titel: `Join the FINGER GROUP`,
             beschrijving: `After you have filled out the form, you will be invited to a telegram or signal group for your specific finger.`,
-            toelichting: `Larger actions sometimes use 'fingers'. These are groups that prepare for the action together and move towards the place of action together. This increases security and our chances of succes.<br/><br/>ADDENDUM: Because this action lasts multiple days, you may be part of multiple fingers for different dates.`,
+            toelichting: ``,
         }
     },
     {
@@ -59,7 +59,7 @@ var data = [
             toelichting: `De actie consensusbevat de basis van alle voorwaarden die we hebben afgesproken als Extinction Rebellion, waaronder geweldloosheid.<br/>Ben je al een meer ervaren rebel? Spreek andere rebellen gerust vriendelijk aan als je ziet dat ze de actieconsensus niet volgen.`,
         },
         engels: {
-            titel: `Read the ACTION CONSESUS`,
+            titel: `Read the ACTION CONSENSUS`,
             beschrijving: `This is very important. Read the action consensus carefully. If you don't agree, DON'T JOIN. <a href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">You can read it here</a>`,
             toelichting: `The Action Consensus contains all the conditions we agreed upon as Extinction Rebellion, including non-violence.<br/>If you're a more experienced rebel, feel free to kindly point it out when you see other rebels going against the action consensus.`,
         }
@@ -68,12 +68,12 @@ var data = [
         id: `b-legal-handout`,
         nederlands: {
             titel: `Lees de LEGAL HANDHOUT`,
-            beschrijving: `De legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM/download/A12-Juridische%20Informatie-FINAL-UPDATEv2.pdf">staat hier</a>`,
+            beschrijving: `De legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">staat hier</a>`,
             toelichting: `Hierin staan antwoorden op vragen zoals: Wat zijn de legale consequenties? Wanneer bel ik een advocaat? Betekent dit iets voor mijn strafblad?`,
         },
         engels: {
             titel: `Read the LEGAL HANDOUT`,
-            beschrijving: `The legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM/download/A12-Juridische%20Informatie-FINAL-UPDATEv2.pdf">can be read here</a-->.`,
+            beschrijving: `The legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">can be read here</a>.`,
             toelichting: `This handout answers questions like: What are the legal consequenses? When do I call a laywer?`,
         }
     },
@@ -107,13 +107,13 @@ var data = [
         id: `b-algemene-briefing`,
         nederlands: {
             titel: `Volg de ALGEMENE BRIEFING`,
-            beschrijving: `Actiebriefing gemist? <a href="https://briefing.a12blokkade.nl/">briefing.a12blokkade.nl</a>`,
-            toelichting: `De algemene briefing geeft een laatste update en herhaalt de belangrijkste punten. Het is een zoom-meeting met honderden deelnemers, dus er is niet veel ruimte voor vragen. Wel kan je deze in je vingergroep vragen.<br/><br/>TOEVOEGING: Voor data ná 9 september zijn er steeds briefings op de avond ervóór.`,
+            beschrijving: `De link naar de algemene actiebriefing wordt in je vinger gedeeld.`,
+            toelichting: `De algemene briefing geeft een laatste update en herhaalt de belangrijkste punten. Het is een online meeting met honderden deelnemers, dus er is niet veel ruimte voor vragen. Wel kan je deze in je vingergroep vragen.<br/>`,
         },
         engels: {
             titel: `Follow the GENERAL BRIEFINGS`,
-            beschrijving: `Missed the briefing? <a href="https://briefing.a12blokkade.nl/">briefing.a12blokkade.nl</a>`,
-            toelichting: `The general briefing provides the most recent updates and repeats the most import points. It's huge zoom meeting, so there's not a lot of time for questions. If you have remaining questions, you can always ask them in your finger group.<br/><br/>ADDENDUM: For dates after September 9th, briefings will be held the evening prior.`,
+            beschrijving: `The link to the general action briefing will be shared in your finger.`,
+            toelichting: `The general briefing provides the most recent updates and repeats the most import points. It's huge online meeting, so there's not a lot of time for questions. If you have remaining questions, you can always ask them in your finger group.<br/>`,
         }
     },
     {
@@ -159,12 +159,12 @@ var data = [
         id: `b-tas-inpakken`,
         nederlands: {
             titel: `Pak je TAS in`,
-            beschrijving: `Een paklijst is <a href="https://cloud.extinctionrebellion.nl/index.php/s/iLB8YA5icBmwNag">hier beschikbaar</a>.`,
+            beschrijving: `Een paklijst is <a href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">hier beschikbaar</a>.`,
             toelichting: `Denk in elk geval aan: warme/droge kleren, eten, WATER, geld, ID-kaart*, OV-kaart*. Géén dierbare bezittingen. Géén wapens, géén drugs.<br/>* niet als je anoniem gaat`,
         },
         engels: {
             titel: `Pack your BAG!`,
-            beschrijving: `A packing list is available <a href="https://cloud.extinctionrebellion.nl/index.php/s/iLB8YA5icBmwNag">here</a>.`,
+            beschrijving: `A packing list is available <a href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">here</a>.`,
             toelichting: `Make sure you at least have enough warm/dry clothing, food, WATER, ID-card*, OV-card*. Don't bring precious things. No alcohol, no drugs.<br/>* not when going anonymously`,
         }
     },
