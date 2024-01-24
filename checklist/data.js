9ac658bd4d4e714dbe6ec4dd550afdad7b917b47
pip-eline <133957473+pip-eline@users.noreply.github.com>
2024-01-24 11:44:23 +0100

    Update data.js

--- checklist/data.js
+++ checklist/data.js
@@ -3,12 +3,12 @@ var data = [
         id: `b-algemene-broadcast`,
         nederlands: {
             titel: `Ga in de ALGEMENE BROADCAST`,
-            beschrijving: `Dat is <a href="https://t.me/A12StopSub">dit telegramkanaal</a> met de titel "A12 StopSub".`,
+            beschrijving: `Dat is <a target="_blank" href="https://t.me/A12StopSub">dit telegramkanaal</a> met de titel "A12 StopSub".`,
             toelichting: `Voor veel acties, en ook voor deze <span class="a-12">A12-actie</span>, is er een grote <em>signal of telegram-groep</em> waar belangrijke mededelingen worden gepost.`,
         },
         engels: {
             titel: `Join the GENERAL BROADCAST`,
-            beschrijving: `It's <a href="https://t.me/A12StopSub">this telegram group</a> with the title "A12 StopSub".`,
+            beschrijving: `It's <a target="_blank" href="https://t.me/A12StopSub">this telegram group</a> with the title "A12 StopSub".`,
             toelichting: `For most actions, including the <em>A12 actions</em>, there is a big broadcast channel where important announcement are made.`,
         }
     },
@@ -16,12 +16,12 @@ var data = [
         id: `b-aanmeldformulier`,
         nederlands: {
             titel: `Vul het AANMELDFORMULIER in`,
-            beschrijving: `Het <a href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">aanmeldformulier</a> zorgt er voor dat je in de juiste groep wordt ingedeeld, voor verdere communicatie.`,
+            beschrijving: `Het <a target="_blank" href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">aanmeldformulier</a> zorgt er voor dat je in de juiste groep wordt ingedeeld, voor verdere communicatie.`,
             toelichting: `In het formulier kan je ook aangeven of je bepaalde rollen wil vervullen, zoals wellbeing.`,
         },
         engels: {
             titel: `Fill in the REGISTRATION FORM`,
-            beschrijving: `The <a href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">registration form</a> makes sure you are assigned to the right group, for further communication.`,
+            beschrijving: `The <a target="_blank" href="https://cryptpad.organise.earth/form/#/2/form/view/kiwdJkgUteALUT9fIuV8uBQS30WIj0URHFbMxFyrOq4/">registration form</a> makes sure you are assigned to the right group, for further communication.`,
             toelichting: `In the form you can also indicate preferences for specific roles, like wellbeing.`,
         }
     },
@@ -29,13 +29,13 @@ var data = [
         id: `b-actietraining`,
         nederlands: {
             titel: `Volg een ACTIETRAINING`,
-            beschrijving: `<a href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Hier staat een overzicht</a> van alle actietrainingen die nog gegeven worden.`,
-            toelichting: `Dit heet ook wel een <em>"non violent direct action training"</em>. Als je dat al eerder hebt gedaan, hoeft dat niet nogmaals. Maar als je nieuw bent, is een actietraining heel belangrijk: doe niet mee zonder actietraining! Je kan dan wel nog meedoen aan de <a href="https://t.me/A12supporters">support demo</a>.`,
+            beschrijving: `<a target="_blank" href="https://extinctionrebellion.nl/events/?organizer&category=Actietraining">Hier staat een overzicht</a> van alle actietrainingen die nog gegeven worden.`,
+            toelichting: `Dit heet ook wel een <em>"non violent direct action training"</em>. Als je dat al eerder hebt gedaan, hoeft dat niet nogmaals. Maar als je nieuw bent, is een actietraining heel belangrijk: doe niet mee zonder actietraining! Je kan dan wel nog meedoen aan de <a target="_blank" href="https://t.me/A12supporters">support demo</a>.`,
         },
         engels: {
             titel: `Do an ACTION TRAINING`,
-            beschrijving: `<a href="https://extinctionrebellion.nl/en/events/?category=Actietraining&organizer">Here is an overview</a> of all the trainings being organised.`,
-            toelichting: `This is also called a <em>"non violent direct action training"</em>. If you've done one before, you don't need to do it again. However, if this is your first action, this training is very important: you shouldn't go without one! Without a training you can still join the <a href="https://t.me/A12supporters">support demo</a>.`,
+            beschrijving: `<a target="_blank" href="https://extinctionrebellion.nl/en/events/?category=Actietraining&organizer">Here is an overview</a> of all the trainings being organised.`,
+            toelichting: `This is also called a <em>"non violent direct action training"</em>. If you've done one before, you don't need to do it again. However, if this is your first action, this training is very important: you shouldn't go without one! Without a training you can still join the <a target="_blank" href="https://t.me/A12supporters">support demo</a>.`,
         }
     },
     {
@@ -43,24 +43,24 @@ var data = [
         nederlands: {
             titel: `Ga in de VINGERGROEP`,
             beschrijving: `Nadat je het formulier hebt ingevuld, wordt je ingedeeld bij een telegram- of signalgroep voor jouw vinger.`,
-            toelichting: ``,
+            toelichting: `Bij grote acties wordt er soms gewerkt met 'vingers': dat zijn groepen die samen voorbereiden en naar de actielocatie lopen. Dit vergroot de veiligheid en de kans dat zo veel mogelijk rebellen op de actie aankomen.`,
         },
         engels: {
             titel: `Join the FINGER GROUP`,
             beschrijving: `After you have filled out the form, you will be invited to a telegram or signal group for your specific finger.`,
-            toelichting: ``,
+            toelichting: `Larger actions sometimes use 'fingers'. These are groups that prepare for the action together and move towards the place of action together. This increases security and our chances of succes.`,
         }
     },
     {
         id: `b-actieconsensus`,
         nederlands: {
             titel: `Lees de ACTIECONSENSUS`,
-            beschrijving: `Dit is heel belangrijk. Lees de actie consensus zorgvuldig. Als je het ergens niet mee eens ben, GA DAN NIET MEE. <a href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">Je kunt het hier lezen.</a>`,
+            beschrijving: `Dit is heel belangrijk. Lees de actie consensus zorgvuldig. Als je het ergens niet mee eens ben, GA DAN NIET MEE. <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">Je kunt het hier lezen.</a>`,
             toelichting: `De actie consensusbevat de basis van alle voorwaarden die we hebben afgesproken als Extinction Rebellion, waaronder geweldloosheid.<br/>Ben je al een meer ervaren rebel? Spreek andere rebellen gerust vriendelijk aan als je ziet dat ze de actieconsensus niet volgen.`,
         },
         engels: {
             titel: `Read the ACTION CONSENSUS`,
-            beschrijving: `This is very important. Read the action consensus carefully. If you don't agree, DON'T JOIN. <a href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">You can read it here</a>`,
+            beschrijving: `This is very important. Read the action consensus carefully. If you don't agree, DON'T JOIN. <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/4WWr9DeXaQamSCJ">You can read it here</a>`,
             toelichting: `The Action Consensus contains all the conditions we agreed upon as Extinction Rebellion, including non-violence.<br/>If you're a more experienced rebel, feel free to kindly point it out when you see other rebels going against the action consensus.`,
         }
     },
@@ -68,12 +68,12 @@ var data = [
         id: `b-legal-handout`,
         nederlands: {
             titel: `Lees de LEGAL HANDHOUT`,
-            beschrijving: `De legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">staat hier</a>`,
+            beschrijving: `De legal handout <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">staat hier</a>`,
             toelichting: `Hierin staan antwoorden op vragen zoals: Wat zijn de legale consequenties? Wanneer bel ik een advocaat? Betekent dit iets voor mijn strafblad?`,
         },
         engels: {
             titel: `Read the LEGAL HANDOUT`,
-            beschrijving: `The legal handout <a href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">can be read here</a>.`,
+            beschrijving: `The legal handout <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/H5BJkpPJTwkEgkM">can be read here</a>.`,
             toelichting: `This handout answers questions like: What are the legal consequenses? When do I call a laywer?`,
         }
     },
@@ -94,12 +94,12 @@ var data = [
         id: `b-anonimiteit`,
         nederlands: {
             titel: `Bepaal: ANONIEM of niet?`,
-            beschrijving: `Er zijn diverse voor- en nadelen dus laat je vooral goed informeren. Voor meer information, vraag de mede-rebellen of de vingercoordinator in je vingergroep. <a href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Hier is ook een document met meer informatie</a>`,
+            beschrijving: `Er zijn diverse voor- en nadelen dus laat je vooral goed informeren. Voor meer information, vraag de mede-rebellen of de vingercoordinator in je vingergroep. <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Hier is ook een document met meer informatie</a>`,
             toelichting: `Als je anoniem gaat, neem dan ook geen spullen mee met jouw naam er op. Dus ook geen bankpas of OV-chipkaart op naam.`,
         },
         engels: {
             titel: `Consider: Will I go ANONYMOUS?`,
-            beschrijving: `There are various pros and cons for anonymity, so inform yourself well. For more information, ask other rebels or the finger coordinator in your finger group. <a href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Here is a document with more information</a>`,
+            beschrijving: `There are various pros and cons for anonymity, so inform yourself well. For more information, ask other rebels or the finger coordinator in your finger group. <a target="_blank" href="https://cloud.extinctionrebellion.nl/index.php/s/S6kdyF5Nyt8xABo">Here is a document with more information</a>`,
             toelichting: `If you go anonymous, don't bring any stuff with your name on it. That also means no debit card or named OV card.`,
         }
     },
@@ -159,12 +159,12 @@ var data = [
         id: `b-tas-inpakken`,
         nederlands: {
             titel: `Pak je TAS in`,
-            beschrijving: `Een paklijst is <a href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">hier beschikbaar</a>.`,
+            beschrijving: `Een paklijst is <a target="_blank" href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">hier beschikbaar</a>.`,
             toelichting: `Denk in elk geval aan: warme/droge kleren, eten, WATER, geld, ID-kaart*, OV-kaart*. Géén dierbare bezittingen. Géén wapens, géén drugs.<br/>* niet als je anoniem gaat`,
         },
         engels: {
             titel: `Pack your BAG!`,
-            beschrijving: `A packing list is available <a href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">here</a>.`,
+            beschrijving: `A packing list is available <a target="_blank" href="https://cryptpad.organise.earth/file/#/2/file/wCBUmSULK2yaPPCaUOfQ4gPq/">here</a>.`,
             toelichting: `Make sure you at least have enough warm/dry clothing, food, WATER, ID-card*, OV-card*. Don't bring precious things. No alcohol, no drugs.<br/>* not when going anonymously`,
         }
     },
@@ -185,13 +185,13 @@ var data = [
         id: `b-watervaste-stift`,
         nederlands: {
             titel: `Schrijf dit op je LICHAAM`,
-            beschrijving: `Schrijf met <em>watervaste stift</em> twee dingen duidelijk op je lichaam:<br/>1. Naam van de advocaat: Willem Jebbink.<br/>2. Je RST-nummer.`,
-            toelichting: `De naam van de advocaat is Willem Jebbink. Een telefoonnummer is niet nodig.`,
+            beschrijving: `Schrijf met <em>watervaste stift</em> twee dingen duidelijk op je lichaam:<br/>1. Naam van de advocaat: Ineke van den Brûle.<br/>2. Je RST-nummer.`,
+            toelichting: `De naam van de advocaat is Ineke van den Brûle. Een telefoonnummer is niet nodig.`,
         },
         engels: {
             titel: `Write this on your BODY`,
-            beschrijving: `Use a permanent marker to write these two things on your body:<br/>1. Name of your lawyer: Willem Jebbink.<br/>2. Your RST-number.`,
-            toelichting: `The name of your laywer is Willem Jebbink. A phone number is not needed.`,
+            beschrijving: `Use a permanent marker to write these two things on your body:<br/>1. Name of your lawyer: Ineke van den Brûle.<br/>2. Your RST-number.`,
+            toelichting: `The name of your laywer is Ineke van den Brûle. A phone number is not needed.`,
         }
     },
     {
